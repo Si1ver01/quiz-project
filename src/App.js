cff308d0801fab6da67520d7ecbcface96794728
Danshin <si1ver300594@gmail.com>
2019-11-12 15:50:36 +0600

    add Layout.js => High order component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
+import Layout from "./components/layout/layout.js";
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <Layout>
+      <h1>Hello world!</h1>
+    </Layout>
   );
 }
 
